169e81c1a6f2d7ef38f8d89717df7b7d26b16893
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2023-01-26 16:58:33 +0900

    プロジェクト ファイルを追加します。

---
+++ ex34_03_igata/Box.cs
@@ -0,0 +1,12 @@
+﻿namespace ShapeSolid
+{
+    internal class Box : Column
+    {
+        public Box(float w, float h, float d) : base(new RectAngle(w, d), h)
+        {
+        }
+        public Box(float w = 1) : base(new RectAngle(w, w), w)
+        {
+        }
+    }
+}
---
+++ ex34_03_igata/Cylinder.cs
@@ -0,0 +1,9 @@
+﻿namespace ShapeSolid
+{
+    internal class Cylinder:Column
+    {
+        public Cylinder(float radius, float height):base(new Circle(radius), height)
+        {
+        }
+    }
+}
